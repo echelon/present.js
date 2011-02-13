454f349e49dc3a91705ffc67a4715a54fefa5e68
Brandon Thomas Suit <brandon.suit@gmail.com>
2011-02-12 22:13:21 -0500

    More OOish.

--- slidemake.js
+++ slidemake.js
@@ -7,16 +7,43 @@ function slidemake(text)
 	var html = '';
 
 	slides = split_into_slides(text);
-	markdown_slides(slides);
-	make_s5_slides(slides);
 
 	for(var i = 0; i < slides.length; i++){
-		html += slides[i]; 
+		html += slides[i].toHtml(); 
 	};
 	return html;
 };
 
 /* =============== NON-PUBLIC API FOLLOWS ============= */
+
+/**
+ * Represents a markdown slide.
+ */
+function Slide(markdown)
+{
+	this._markdown = markdown;
+	this._html = '';	
+
+	if(typeof Slide.convert == 'undefined') {
+		Slide.convert = new Showdown.converter();
+	};
+
+	this.getMarkdown = function() { return this._markdown; };
+
+	this.setMarkdown = function(markdown) {
+		this._markdown = markdown;
+		this._html = '';
+	};
+
+	this.toHtml = function() {
+		if(!this._html) {
+			this._html = '<div class="slide">';
+			this._html += Slide.convert.makeHtml(this._markdown);
+			this._html += '</div>';
+		}
+		return this._html;
+	};
+};
 
 /**
  * Split a markdown document into slides.
@@ -28,29 +55,14 @@ function split_into_slides(text)
 	// Lookahead ensures that this occurs only at a title (as 
 	// designated by arbitrary title text followed by at least 
 	// four underlining ='s on the following line).
-	var regex = /[\n|\r]{3,}(?=[^\n\r]+[\n|\r]{1,2}={4,})/m;
-	var slides = text.split(regex);
-	for(var i=0; i < slides.length; i++) {
-		slides[i] += "\n\n";	
+	// XXX: REGEX FAILS UNDER LINUX...
+	//var regex = /[\n|\r]{3,}(?=[^\n\r]+[\n|\r]{1,2}={4,})/m;
+	var regex = /([\n|\r]{3,}|\n{2,})(?=[^\n\r]+[\n|\r]{1,2}={4,})/m;
+	var s = text.split(regex);
+	var slides = [];
+	for(var i=0; i < s.length; i++) {
+		slides.push(new Slide(s[i] + "\n\n"));	
 	};
 	return slides;
 };
 
-/**
- * Convert the markdown-formatted 'slides'. 
- */
-function markdown_slides(slides) {
-	var c = new Showdown.converter();
-	for(var i=0; i < slides.length; i++) {
-		slides[i] = c.makeHtml(slides[i]);	
-	};
-};
-
-/**
- * Wrap each slide in S5 slide markup.
- */
-function make_s5_slides(slides) {
-	for(var i=0; i < slides.length; i++) {
-		slides[i] = '<div class="slide">' + slides[i] + '</div>';
-	}
-};
