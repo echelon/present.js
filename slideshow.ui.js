8283d5338a9080862452919227bc16f0b7fbf57a
Brandon Thomas Suit <brandon.suit@gmail.com>
2011-03-06 04:43:37 -0500

    Added clock, styles, page formatting...

--- slideshow.ui.js
+++ slideshow.ui.js
@@ -4,6 +4,26 @@
  */
 var SS = null;
 
+// TODO: Rename init()
+function handleLoad(data) {
+	SS = slidemake3(data);
+	show();
+
+	var timer = setInterval(updateClock, 1000*10); // Updates every ten seconds 
+	updateClock();
+
+	// Key bindings
+	$(document).bind('keydown', 'right', function() { next(); }); 
+	$(document).bind('keypress', 'j', function() { next(); }); // Vim-like
+	$(document).bind('keypress', 'l', function() { next(); }); // Vim-like
+	$(document).bind('keydown', 'left', function() { prev(); }); 
+	$(document).bind('keypress', 'k', function() { prev(); }); // Vim-like
+	$(document).bind('keypress', 'h', function() { prev(); }); // Vim-like
+
+	// Page Layout
+	$(window).resize(function() { $('.presentation').center(); });
+};
+
 function next() {
 	SS.next();
 	show();
@@ -22,7 +42,7 @@ function show() {
 	$('#header').html(h);
 
 	// TODO: Move this. 
-	$('#footer').html('Slide #' + SS.curSlide() + ' of ' + SS.totalSlides());
+	$('#page').html('Slide #' + SS.curSlide() + ' of ' + SS.totalSlides());
 	
 	$('.presentation').center();	
 };
@@ -31,3 +51,31 @@ function Header() {
 	// TODO	
 
 };
+
+function updateClock() {
+	var d = new Date();
+	function zerofill(n) {
+		return (n>9)? n : "0" + n;
+	};
+
+	$('#time').html(d.getHours()+':'+zerofill(d.getMinutes()));
+};
+
+// From http://stackoverflow.com/questions/210717/what-is-the-best-
+// way-to-center-a-div-on-the-screen-using-jquery/210733#210733
+jQuery.fn.center = function () {
+	this.css("position","absolute");
+	this.css("top", ( $(window).height() - this.height() ) / 2+$(window).scrollTop() + "px");
+	this.css("left", ( $(window).width() - this.width() ) / 2+$(window).scrollLeft() + "px");
+	return this;
+};
+
+function loadfile() {
+	var file = $(document).getUrlParam('mkd') || 'example.mkd';
+	$.ajax({
+			url: file,
+			dataType: 'text',
+			success: function(data) { handleLoad(data); },
+	});
+};
+
